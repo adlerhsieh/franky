3deb0f504f813fd2c0650c30454b061c89076b64
Adler <nkj20932@hotmail.com>
2015-01-27 08:30:06 +0800

    add command options for minimal template

--- lib/franky/generator.rb
+++ lib/franky/generator.rb
@@ -5,14 +5,17 @@ require 'colorize'
 module Franky
 	class Generator < Thor
 
-		desc "min", "creates a project with minimal setup"
-		def min
-			dir_name = "test"
+		desc "new [dir_name] [options]", "creates a project with minimal setup"
+		option :minimal, type: :boolean, default: false, aliases: "-m"
+		def new dir_name
 			if Dir.exist? dir_name
 				puts "Directory with name '#{dir_name}' already exists. Please rename.".red
-			else
+				return
+			end
+			
+			if options[:minimal]
 				FileHandler.new.minimal_template dir_name
 			end
-		end		
+		end
 	end
 end
