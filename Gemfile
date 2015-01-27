8f86d0011cc9c3dbf60e109fe39e73a03ae93975
Adler <nkj20932@hotmail.com>
2015-01-27 11:04:29 +0800

    remove rspec

--- Gemfile
+++ Gemfile
@@ -2,6 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in franky.gemspec
 gemspec
-gem 'rspec'
 gem 'thor'
 gem 'colorize'
--- spec/franky_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'franky'
-
-describe Franky do
-	it "example" do
-		ARGV = ["new", "hello"]
-		Franky::Base.start ARGV
-		expect(Dir.exists? "hello").to be_truthy
-	end
-end
