3c7b9f0e620293013dc8d39d01099f0cb0f151b5
Adler <nkj20932@hotmail.com>
2015-01-27 12:32:04 +0800

    refactoring file copy

--- lib/franky/file_handler.rb
+++ lib/franky/file_handler.rb
@@ -1,34 +1,33 @@
 class FileHandler
-	def self.read file_name
-		File.read(File.expand_path("../../files/#{file_name}", __FILE__))
-	end
 
-	def self.create dir, file, content
-		File.open("#{dir}/#{file}", 'w+') {|f| f.write(content)}
-	end
+	# def self.read file_name
+	# 	File.read(File.expand_path("../../files/#{file_name}", __FILE__))
+	# end
 
-	def prepare_file_contents
-		@config_ru = FileHandler.read "config.ru"
-		@gemfile = FileHandler.read "Gemfile"
-		@app_rb = FileHandler.read "app.rb"
+	# def self.create dir, file, content
+	# 	File.open("#{dir}/#{file}", 'w+') {|f| f.write(content)}
+	# end
+
+	def self.copy dir_name, file_name
+		FileUtils.cp File.expand_path("../../files/#{file_name}", __FILE__), "#{dir_name}/#{file_name}"
 	end
 
 	def minimal_template dir_name
-		prepare_file_contents
 		puts "Creating minimal Sinatra project '#{dir_name}'..."
 
 		Dir.mkdir dir_name
 		puts "Created ".green + "#{dir_name}/"
 
-		FileHandler.create dir_name, "config.ru", @config_ru
+		FileHandler.copy dir_name, "config.ru"
 		puts "Created ".green + "#{dir_name}/config.ru"
 
-		FileHandler.create dir_name, "Gemfile", @gemfile
+		FileHandler.copy dir_name, "Gemfile"
 		puts "Created ".green + "#{dir_name}/Gemfile"
 
-		FileHandler.create dir_name, "app.rb", @app_rb
+		FileHandler.copy dir_name, "app.rb"
 		puts "Created ".green + "#{dir_name}/app.rb"
 
-		puts "Done!"
+		puts "Done!".green
 	end
+
 end
