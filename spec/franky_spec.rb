e3590088bddb16fbcc957270032e9fbbb825b339
Adler <nkj20932@hotmail.com>
2015-01-27 11:00:58 +0800

    remove fakefs and add rake commands

--- spec/franky_spec.rb
+++ spec/franky_spec.rb
@@ -1,9 +1,9 @@
 require 'franky'
 
-FakeFS do
-	describe Franky do
-		it "example" do
-			expect(Franky::Base.new).to eq(1)
-		end
+describe Franky do
+	it "example" do
+		ARGV = ["new", "hello"]
+		Franky::Base.start ARGV
+		expect(Dir.exists? "hello").to be_truthy
 	end
 end
